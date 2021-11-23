7e2b122fe75147aa0720f25b2a2d98596d0a2ddb
ayshabintmahmud <aysherbintmahmud@gmail.com>
2021-11-23 21:51:47 +0100

    FooderLich App

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'fooderlich_theme.dart';
+import 'home.dart';
+
+void main() {
+  runApp(const Fooderlich());
+}
+
+class Fooderlich extends StatelessWidget {
+  const Fooderlich({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    //Create theme
+    final theme = FooderlichTheme.dark();
+    //return Home widget from home.dart
+    return MaterialApp(
+      theme: theme,
+      title: 'Fooderlich',
+      home: const Home(),
+    );
+  }
+}
