d330792c6c2110eb6368081d658bc4d6611e86af
ayshabintmahmud <aysherbintmahmud@gmail.com>
2021-11-30 07:14:00 +0100

    To Buy Tab

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'package:provider/provider.dart';
+import 'models/models.dart';
 import 'fooderlich_theme.dart';
 import 'home.dart';
 
@@ -9,13 +10,23 @@ void main() {
 
 class Fooderlich extends StatelessWidget {
   const Fooderlich({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     final theme = FooderlichTheme.light();
     return MaterialApp(
       theme: theme,
       title: 'Fooderlich',
-      home: const Home(),
+      //  MultiProvider
+      // 1
+      home: MultiProvider(
+        providers: [
+          // 2
+          ChangeNotifierProvider(create: (context) => TabManager()),
+          // TODO 10: Add GroceryManager Provider
+        ],
+        child: const Home(),
+      ),
     );
   }
 }
---
+++ lib/screens/grocery_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'empty_grocery_screen.dart';
+
+class GroceryScreen extends StatelessWidget {
+  const GroceryScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // EmptyGroceryScreen
+    // TODO 4: Add a scaffold widget
+    return const EmptyGroceryScreen();
+  }
+  // TODO: Add buildGroceryScreen
+}
